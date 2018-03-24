7a8fc3dfc20c238795946896fd45330af172f61d
John W. Fail <john@pixelache.ac>
2018-03-24 12:13:11 +0200

    one hour grace period on events

--- app/themes/kuusipalaa/js/user_functions.js
+++ app/themes/kuusipalaa/js/user_functions.js
@@ -27,7 +27,7 @@ ipcRenderer.on('load-user-info-2', (event, data) => {
 ipcRenderer.on('load-events', (event, data) => {
   $('#events').html('');
   let now = new Date();
-
+  const HOUR = 1000 * 60 * 60;
   // console.log('events are ' + JSON.stringify(data));
   for (let i = 0; i < data.length; i++) {
     $('#events').append($('<button></button>').
@@ -39,11 +39,11 @@ ipcRenderer.on('load-events', (event, data) => {
         .append($('<div class="so_far"></div').text(data[i].attributes["checked_in_so_far"] + ' checked in today'))
         .append($('<div class="temps"></div').text(data[i].attributes["cost_bb"] + "ᵽ"))
           ).append($('<br />'));
-          if (new Date(data[i].attributes["end_at"] + 1.hour) < now)  {
+          if (new Date(new Date(data[i].attributes["end_at"]) + HOUR) < now)  {
             $('#' + data[i].attributes.slug).addClass('inactive').attr('disabled', 'disabled');
             $('#' + data[i].attributes.slug + " .title").append($('<div></div>').attr({class: 'inactive'}).text('ended at ' + new Date(data[i].attributes["end_at"]).toLocaleTimeString().replace(/:\d+$/, ' ')));
 
-          } else if (new Date(data[i].attributes["start_at"] - 1.hour) > now)  {
+          } else if (new Date(new Date(data[i].attributes["start_at"]) - HOUR) > now)  {
             $('#' + data[i].attributes.slug).addClass('inactive').attr('disabled', 'disabled');
             $('#' + data[i].attributes.slug + " .title").append($('<div></div>').attr({class: 'inactive'}).text('starts at ' + new Date(data[i].attributes["start_at"]).toLocaleTimeString().replace(/:\d+$/, ' ')));
 
